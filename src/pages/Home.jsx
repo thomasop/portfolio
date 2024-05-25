9e2b86cbec132307e0aba0aeb4f17c41039bfe6a
thomasop <tdss33@hotmail.com>
2024-05-25 12:34:10 +0200

    Add: rules screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Board from "../components/home/board/Board";
 import styles from "./Home.module.scss";
 import GameContext from "../providers/game/Context";
 import Wait from "../components/home/wait/Wait";
+import Rules from "../components/home/rules/Rules";
 
 const Home = () => {
   const { value } = useContext(GameContext);
@@ -11,6 +12,7 @@ const Home = () => {
       <h1 className={styles.home__title}>snake game</h1>
       {value.start === true && <Board />}
       {value.start === false && value.rules === false && <Wait />}
+      {value.start === false && value.rules === true && <Rules />}
     </div>
   );
 };
